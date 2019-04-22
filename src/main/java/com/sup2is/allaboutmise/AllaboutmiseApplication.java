edf1995c648e21767373b5693ff7d52c0aa32ac9
sup2is <dev.sup2is@gmail.com>
2019-04-22 17:56:29 +0900

    initial commit

---
+++ src/main/java/com/sup2is/allaboutmise/AllaboutmiseApplication.java
@@ -0,0 +1,13 @@
+package com.sup2is.allaboutmise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllaboutmiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllaboutmiseApplication.class, args);
+	}
+
+}
