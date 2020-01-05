e8c3257fbf08fb958dc60f550f88c031d29690a7
sup2is <dev.sup2is@gmail.com>
2020-01-05 15:00:24 +0900

    리팩터링
    Xml parsing 중 encoding utf로 변경

--- src/main/java/com/sup2is/allaboutmise/service/MiseServiceImpl.java
+++ src/main/java/com/sup2is/allaboutmise/service/MiseServiceImpl.java
@@ -1,31 +1,19 @@
 package com.sup2is.allaboutmise.service;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.util.List;
-
-import javax.xml.parsers.SAXParser;
-
+import com.sup2is.allaboutmise.model.Mise;
 import com.sup2is.allaboutmise.util.ApiRequestUtil;
+import com.sup2is.allaboutmise.util.XmlParserHandler;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
 
-import com.sup2is.allaboutmise.model.Mise;
-import com.sup2is.allaboutmise.util.XmlParserHandler;
-
-import lombok.extern.slf4j.Slf4j;
+import javax.xml.parsers.SAXParser;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -41,10 +29,9 @@ public class MiseServiceImpl implements MiseService {
 	@Cacheable(value = "mise-realtime", key="#cityName")
 	public List<Mise> getCachedRealTimeMiseListByCityName(String cityName) throws Exception {
 		ResponseEntity<String> response = apiRequestUtil.requestAirPollutionData(cityName);
-		InputStream is = new ByteArrayInputStream(response.getBody().getBytes());
+		InputStream is = new ByteArrayInputStream(response.getBody().getBytes("UTF-8"));
 		parser.parse(is, xmlParserHandler);
 		List<Mise> miseList = xmlParserHandler.getParsedData();
-		System.out.println(miseList);
 		return miseList;
 	}
 
